d56d4cd4c6fc3c3cc9631160430e0ec79292960e
Abd <Abd@gmail.com>
2025-05-11 23:30:49 +0300

    create property folder on the first_branch_on_Abd_lab but didn't merge it

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PropertyModule } from './property/property.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PropertyModule],
   controllers: [AppController],
   providers: [AppService],
 })
